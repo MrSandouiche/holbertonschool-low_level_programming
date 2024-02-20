6a31b439bd80642efed04b087b0d4ef722f52758
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-20 14:23:36 +0000

    du c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
-#ifdef _WIN64
-#define ARCHITECTURE "64-bit"
-#elif _WIN32
-#define ARCHITECTURE "32-bit"
-#else
-#define ARCHITECTURE "unknown"
-#endif
  int main (void)
   
 {
