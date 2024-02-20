469823abadb11072cd7a83503d093cbe0c028ec3
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-20 09:35:18 +0000

    du c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char string1[] = "\"Programming is like building a multilingual puzzle";
+		char string1[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(string1);
-	return (0);
+		puts(string1);
+		return (0);
 }
