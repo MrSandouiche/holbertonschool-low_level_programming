7a11aee99d0f7bde7a86c7fdd559964ad5853e69
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-20 13:48:44 +0000

    du c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,7 +2,9 @@
  int main (void)
   
 {
-   printf ("Size of char : %ld byte\n", sizeof (char));
-   printf ("Size of int : %ld byte\n", sizeof (int));
+	printf ("Size of char : %ld byte\n", sizeof (char));
+	printf ("Size of int : %ld byte\n", sizeof (int));
+	printf ("Size of long int : %ld byte\n", sizeof (long int));
+	printf ("Size of a float : %ld byte\n", sizeof (float));
 return(0);
 }
