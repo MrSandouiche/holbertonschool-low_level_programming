53b875524b6f40cb3fb613e69ee5e7df933aa177
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-20 13:15:26 +0000

    du c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-char *variabletype[] = {"char", "unsigned char", "signed char", "int", "unsigned int", "short", "unsigned short", "long", "unsigned long", "long long", "unsigned long long"};
-
-int main() {
-    for (int i = 0; i < 11;++i) {
-        printf("Size of %s is %u\n",variabletype[i], (unsigned int)(sizeof(variabletype[i])));
-    }
-    return 0;
+int main(void)
+{
+	printf("Size of char : %Id byte\n",sizeof(char));
+Return(0)
 }
