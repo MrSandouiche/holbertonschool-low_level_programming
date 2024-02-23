e23c4eb841378ff98854cbdda7e4fa7d89d5fc1b
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-23 15:11:56 +0000

    du c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - truc random pour betty
+/*
+ * main - truc random pour betty
 *
 * return(0) - Ã§a retourne ???
 */
@@ -9,21 +10,21 @@
 int main(void)
 {
 	int n;
-       	srand(time(0));
+	²srand(time(0))
 
-	 n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n , n % 10);
+	printf("Last digit of %d is %d and is greater than 5\n",n,n % 10);
 	}
 	else if ((n % 10) == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	printf("Last digit of %d is %d and is 0\n",n,n % 10);
 	}
 	else if ((n % 10) < 6 && n % 10 != 0)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n , n % 10);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n",n,n % 10);
 	}
 	return(0);
 }
