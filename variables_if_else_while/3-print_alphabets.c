801bdb65d12685897baccadadac5206c80ba74f4
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-25 16:31:55 +0000

    du c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char i;
+
+        for (i = 'a'; i <= 'z'; i++)
+        {
+                putchar(i);
+        }
+        for (i = 'A' ; i <= 'Z'; i++)
+        {
+            putchar (i);
+        }
+
+        putchar('\n');
+
+        return (0);
+}
