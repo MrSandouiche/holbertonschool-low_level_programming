e77476934a7dfc1a06af099ba87d8409e06ea4af
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-25 18:16:54 +0000

    du c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Ãa imprime avec une boucle les nombre de 0 a 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	for (int n = 0; n < 10;  n++)
+	printf("%d", n);
+return (0);
+}
