05088e18f046102f9616e22938b2ec0e8dd901ce
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-25 17:15:45 +0000

    du c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-        char i;
+	char i;
 
-        for (i = 'a'; i <= 'z'; i++)
-        {
-            if (i != 'q' && i != 'e' )
-                putchar(i);
-        }
-               putchar('\n');
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		putchar(i);
+	}
+		putchar('\n');
 
-        return (0);
+	return (0);
 }
