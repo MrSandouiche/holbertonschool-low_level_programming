4201d0e36ee6a7863b445fa593b0e5fc6e31a3ec
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-25 18:28:04 +0000

    du c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
+int main(void) {
+	int n;
+	for (n = 0; n < 10; n++) {
 
-	for (int n = 0; n < 10;  n++)
-	printf("%d", n);
+	printf("%d\n",n);
+}	
 return (0);
 }
