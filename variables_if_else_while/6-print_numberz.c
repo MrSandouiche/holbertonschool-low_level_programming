0d19d0fd2f994106307d3b23ada6e795bdce161b
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-25 20:13:21 +0000

    du c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-        int n;
-        
-        for (n = 0; n < 10; n++)  {
+int main(void)
+{
+	int n;
 
-        putchar(n + '0');
-        }
-        putchar('\n');
+	for (n = 0; n < 10; n++)
+	{
+
+	putchar(n + '0');
+	}
+	putchar('\n');
 return (0);
 }
