b46ad26ec720fe0d894dcce8de13a5b4f28bf0cc
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-25 16:35:05 +0000

    du c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,18 +9,18 @@
  */
 int main(void)
 {
-        char i;
+	char i;
 
-        for (i = 'a'; i <= 'z'; i++)
-        {
-                putchar(i);
-        }
-        for (i = 'A' ; i <= 'Z'; i++)
-        {
-            putchar (i);
-        }
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+		for (i = 'A' ; i <= 'Z'; i++)
+	{
+		putchar (i);
+	}
 
-        putchar('\n');
+		putchar('\n');
 
-        return (0);
+	return (0);
 }
