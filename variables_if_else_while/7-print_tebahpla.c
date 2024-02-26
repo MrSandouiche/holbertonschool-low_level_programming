d2547e16a147e288b58fbd8a773d1a6adc9305ae
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-26 14:02:04 +0000

    du c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: ça imprime l'alphabet MAIS a l'envers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	putchar(i);
+	}
+
+return (0);
+}
