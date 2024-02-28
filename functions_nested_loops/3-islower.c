fad9b5bde35a1e047065e2e6bd6d0a7c78110d1c
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-28 23:35:05 +0000

    du c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _islower - regarde si le char est en lowercase
+ * @c: le char a verifier
+ *
+ * Return: 1 si c'est en lower sinon 0 .
+ */
 
 int _islower(int c)
 
@@ -7,7 +12,7 @@ int _islower(int c)
 
 	if (c >= 97 && c <= 122)
 {
-		return 1;
+		return (1);
 }
-	return 0;
+	return (0);
 }
