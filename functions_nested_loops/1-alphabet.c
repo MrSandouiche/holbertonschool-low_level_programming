125beb09e43ed9df9a43d745e40f6e3fe57cf7f3
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-28 13:20:59 +0000

    du c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ *print_alphabet - imprime l'alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+	char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+
+}
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
