e7343c99251ac49a8e65d5fb12bab633e3e7991e
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-28 14:07:34 +0000

    du c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_alphabet_x10 - imrime dix fois
+*
+*/
+
+void print_alphabet_x10(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+
+{
+_putchar(i);
+}
+_putchar('\n');
+}
