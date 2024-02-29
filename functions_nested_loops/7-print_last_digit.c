22cd04350dc3daf83f438e772b967bbd01983c3a
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-29 06:16:25 +0000

    du c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,22 @@
 #include "main.h"
+/**
+ * print_last_digit - print le dernier chiffre avec modulo
+ * @n: le nombre que j'imprime
+ *
+ * Return: le dernier nombre de n
+ */
 
+	int print_last_digit(int n)
+{
+		int last_digit;
 
-int print_last_digit(int);
-	int last_digit
-		
-	last_digit = %10
-	return last_digit
+	last_digit = n % 10;
+
+		if (last_digit < 0)
+			last_digit = -last_digit;
+
+		_putchar('0' + last_digit);
+
+		return (last_digit);
+
+}
