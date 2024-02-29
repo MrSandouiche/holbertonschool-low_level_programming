9c7099744c4c1bbee4a3ee4ded1671b23523ccfd
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-29 04:35:03 +0000

    du c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - calcul la valeur de l'absolue de l'integre
+ * @n: int qui int
+ *
+ * Return: la valeur absolu de l'absol
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (-n);
+	}
+	else
+{
+	return (n);
+	}
+}
