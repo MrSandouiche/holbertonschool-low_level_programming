0501ea370c9842408ff0caeec4bc654287fc2e10
MrSandouiche <zemihimehdi1@gmail.com>
2024-02-29 01:20:38 +0000

    du c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - regarde si le char est en jsp il est 2h jsuis epuisé
+ * @c: le char a verifier
+ *
+ * Return: 1 si c'est en lower sinon 0 .
+ */
+
+
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+
+
+	return (1);
+}
+else
+{
+
+	return (0);
+}
+return (0);
+}
