6d8d19f35e7a14cd93e2b146884a222d86a57156
MrSandouiche <zemihimehdi1@gmail.com>
2024-03-01 11:21:09 +0000

    du c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * _isupper - permet de check si c'est en maj ou pas
+ *
+ *
+ */
+
+
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+		return (0);
+}
