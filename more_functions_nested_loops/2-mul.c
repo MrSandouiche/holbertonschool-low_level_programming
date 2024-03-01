01640c10a1fac9b78ba6b9c8a700dcf738ca0df7
MrSandouiche <zemihimehdi1@gmail.com>
2024-03-01 13:17:45 +0000

    du c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplie le a et b
+ *
+ * Description: Cette fonction permet de multiplier
+ *
+ *@a: la premiere valeur a multiplier
+ *
+ *@b: la deuxieme a multiplier aussi
+ *
+ * Return: multiplie
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
