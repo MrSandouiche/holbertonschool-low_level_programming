442203ab7dbfbc03a365abc4cad649210fce5ca8
MrSandouiche <zemihimehdi1@gmail.com>
2024-03-01 14:06:08 +0000

    du c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,22 @@
 #include "main.h"
-
-
-
-
-
-
+/**
+ * print_numbers - permet de print les nombres avec une boucle for
+ *
+ *
+ * Return: sans nombre parce pourquoi pas
+ */
 
 void print_numbers(void)
 {
 	int e;
-	for(e = 48; e <= 57; e++)
+
+	for (e = 48; e <= 57; e++)
+
 	{
 		_putchar(e);
+	}
+	{
+		_putchar('\n');
 	}
 return;
 }
