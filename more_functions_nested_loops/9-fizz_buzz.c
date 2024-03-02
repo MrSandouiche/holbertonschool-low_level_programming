41dcd30aa6e5a290a7133117a380e45c2dbcbd09
MrSandouiche <zemihimehdi1@gmail.com>
2024-03-02 18:42:58 +0000

    du c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,47 +1,27 @@
 #include <stdio.h>
+#include <stdio.h>
 
-int main() {
-    int i;
+int main() 
+{
+	int i;
     for (i = 1; i <= 100; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
-            putchar('F');
-            putchar('i');
-            putchar('z');
-            putchar('z');
-            putchar('B');
-            putchar('u');
-            putchar('z');
-            putchar('z');
+            printf("FizzBuzz");
         } else if (i % 3 == 0) {
-            putchar('F');
-            putchar('i');
-            putchar('z');
-            putchar('z');
+            printf("Fizz");
         } else if (i % 5 == 0) {
-            putchar('B');
-            putchar('u');
-            putchar('z');
-            putchar('z');
+            printf("Buzz");
         } else {
-            int num = i;
-            int digits = 0;
-            while (num != 0) {
-                digits++;
-                num /= 10;
-            }
-            num = i;
-            while (digits-- > 0) {
-                putchar('0' + (num % 10));
-                num /= 10;
-            }
+            printf("%d", i);
         }
 
-        /* Ajouter un espace sauf pour le dernier nombre */
+       
         if (i != 100) {
-            putchar(' ');
+            printf(" ");
         }
     }
-    putchar('\n');
+    printf("\n");
 
     return 0;
 }
+
