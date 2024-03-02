09fa39771c40dab659271b3add89110b514a923b
MrSandouiche <zemihimehdi1@gmail.com>
2024-03-02 19:19:08 +0000

    du c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size) {
         return;
     }
 
-    for (i = 1; i <= size; i++) {
+    for (i = size; i >= 1; i--) {
         for (j = 1; j <= i; j++) {
             _putchar('#');
         }
