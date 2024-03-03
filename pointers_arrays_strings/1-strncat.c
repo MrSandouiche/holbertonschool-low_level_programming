f218e516756fb8bcf663cb452e9d0c1e6dda6d8d
MrSandouiche <zemihimehdi1@gmail.com>
2024-03-03 07:03:44 +0000

    du c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+/**
+ * _strncat - Concatène deux chaînes de caractè
+ * @dest: Chaîne de destinatin
+ * @src: Chaîne sourc
+ * @n: Nombre maximal de caractère
+ *
+ * Description: Cette fonction concatène la chaîne s
+ *
+ * Return: Un pointeur vers la chaîne de destination (dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	while (dest[dest_len] != '\0')
+	dest_len++;
+
+	while (src[i] != '\0' && i < n)
+	{
+	dest[dest_len + i] = src[i];
+	i++;
+	}
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
