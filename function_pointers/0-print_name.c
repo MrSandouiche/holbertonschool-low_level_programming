066a510ef3a5f71b4fc600773681829595e9f6a6
MrSandouiche <zemihimehdi1@gmail.com>
2024-03-19 14:11:22 +0000

    pointeur fonctions

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * print_name - print un nom
+ * @name: le nom a print
+ * @f le pointeur de la fonction
+ *
+ * Return: aucun
 
 
 void print_name(char *name, void (*f)(char *))
